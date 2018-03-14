b41fc7cd44d5ed78deceb7ee228ded2f39e3e08d
Jin XinChen <jinxinchen@ymail.com>
2018-03-14 00:46:36 -0400

    angular-cli

---
+++ angular/angular-myself/translate.bat
@@ -0,0 +1,6 @@
+cd dashboard
+rename polyfills.*.bundle.js polyfills.bundle.js
+rename main.*.bundle.js main.bundle.js
+rename styles.*.bundle.css styles.bundle.css
+rename vendor.*.bundle.js vendor.bundle.js
+rename inline.*.bundle.js inline.bundle.js
