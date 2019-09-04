4d50efa04b4644be3a7b3dd9633d37952f5b1fda
Jin XinChen <jinxinchen@ymail.com>
2019-09-04 15:27:40 -0400

    the part of CSS
    //上下margin 有重叠效果
    //
    border-1px($color)
      position: relative
      &:after
        display: block
        position: absolute
        left: 0
        bottom: 0
        width: 100%
        border-top: 1px solid $color
        content: ' '
    
    border-none()
      &:after
        display: none
    
    bg-image($url)
      background-image: url($url+"@2x.png")
      @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-rati 3)
        background-image: url($url+"@3x.png")

--- vuejs/sell/src/components/goods/goods.vue
+++ vuejs/sell/src/components/goods/goods.vue
@@ -14,20 +14,20 @@
         <li v-for="item in goods" :key="item.index" class="food-list">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" :key="food.index" class="food-item">
+            <li v-for="food in item.foods" :key="food.index" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon">
+                <img width="57" :src="food.icon">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月{{food.seller}}</span>
+                  <span class="count">月{{food.seller}}</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="new">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -113,4 +113,51 @@
           font-size: 12px
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: grb(147,153,159)
+        background:#f5f3f7
+      .food-item
+        display: flex
+        margin: 18px //上下margin 有重叠效果
+        padding-bottom 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px;
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right:12px
+          .price
+            font-size: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
