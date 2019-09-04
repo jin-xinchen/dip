dfddee41ff40150528293dd87858e8f1b9bd4fe3
Jin XinChen <jinxinchen@ymail.com>
2019-09-04 14:04:56 -0400

    menu-item
    right side menu-item

--- vuejs/sell/src/components/goods/goods.vue
+++ vuejs/sell/src/components/goods/goods.vue
@@ -2,8 +2,8 @@
   <div class="goods">
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods" :key="item.index" clss="menu-item">
-          <span class="text">
+        <li v-for="item in goods" :key="item.index" class="menu-item">
+          <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
@@ -43,6 +43,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixin";
   .goods
     display: flex
     position: absolute
@@ -54,6 +55,37 @@
       flex: 0 0 80px
       width: 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        line-height: 14px
+        padding: 0 12px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          color: red
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 12px
     .foods-wrapper
       flex: 1
-</style> 
+</style>
